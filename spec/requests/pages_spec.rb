27ffc940a177d9a530792f7dc37f957fb7bbfe00
Shaun <smartin@meiresearch.org>
2013-07-11 08:40:19 -0500

    Update rspec to use  and add contact page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,42 +3,56 @@ require 'spec_helper'
 describe "Static Pages" do
   describe "Home Page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/pages/home'
       page.should have_selector('h1', text: 'Sample App')
     end
 
     it "should have the right title" do
       visit '/pages/home'
-      page.should have_selector('title', text: 'Sample App | Home')
+      page.should have_title('Home')
     end
 
   end  
 
   describe "Help Page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/pages/help'
       page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the right title" do
       visit '/pages/help'
-      page.should have_selector('title', text: 'Sample App | Help')
+      page.should have_title('Help')
     end
 
   end 
 
   describe "About Page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit '/pages/about'
       page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the right title" do
       visit '/pages/about'
-      page.should have_selector('title', text: 'Sample App | About Us')
+      page.should have_title('About Us')
+    end
+
+  end 
+
+  describe "Contact Page" do
+
+    it "should have the h1 'Contact Us'" do
+      visit '/pages/contact'
+      page.should have_selector('h1', text: 'Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/pages/contact'
+      page.should have_title('Contact Us')
     end
 
   end 
