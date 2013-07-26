287cdf16e5d07f3fec392bee440a510f61c3e62a
Shaun <smartin@meiresearch.org>
2013-07-26 12:02:23 -0500

    Add user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,21 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def new
+    @user = User.new
   end
 
   def show
     # raise params.inspect
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   private
